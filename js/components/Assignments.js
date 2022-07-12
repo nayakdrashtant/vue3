4062af1e72e5b2a9c71fc2e5a575441b7c989594
Drash Nayak <drashtantnayak@dal.ca>
2022-07-12 10:58:21 -0300

    learn/lifecycle-hooks-fake-apis-and-ajax

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -15,12 +15,16 @@ export default {
     },
     data() {
         return {
-            assignments: [
-                {name: 'Finish project', complete: false, id: 1, tag: 'science'},
-                {name: 'Read chapter 4', complete: false, id: 2, tag: 'math'},
-                {name: 'Turn in homework', complete: false, id: 3, tag: 'math'},
-            ],
+            assignments: [],
         }
+    },
+
+    created() {
+        fetch('http://localhost:3001/assignments')
+            .then(response => response.json())
+            .then(assignments => {
+                this.assignments = assignments;
+            })
     },
 
     methods: {
